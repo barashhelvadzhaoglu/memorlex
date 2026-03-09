16768f6b85fd0fae5e6f0399e7873505e314a8dd
Barash Helvadzhaoglu <barashhelvadzhaoglu@gmail.com>
2026-03-09 23:44:37 +0100

    🔧 run script ve plist path'leri güncellendi

--- run_memorlex_de.sh
+++ run_memorlex_de.sh
@@ -4,15 +4,15 @@ unset PYTHONPATH
 unset PYTHONHOME
 export PYTHONUNBUFFERED=1
 
-cd /Users/user
+cd /Users/user/memorlex
 
 LOG="/Users/user/memorlex/memorlex_de.log"
-ERR="/Users/user/memorlex_de_error.log"
+ERR="/Users/user/memorlex/memorlex_de_error.log"
 
 echo "$(date): Pipeline başladı" >> "$LOG"
 
 /Users/user/memorlex_venv2/bin/python3 -u \
-    /Users/user/memorlex_scripts/de/master_run.py \
+    /Users/user/memorlex/scripts/de/master_run.py \
     >> "$LOG" 2>> "$ERR"
 
 echo "$(date): Pipeline bitti" >> "$LOG"
